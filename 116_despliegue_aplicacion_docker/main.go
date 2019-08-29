5ff649d6d8394d32987bf398364853a6957870a7
Arturo Maltos <m4ltos@gmail.com>
2019-08-29 01:13:23 -0400

    Agregado ejercicio 116 despliegue de app utilizando docker.

---
+++ 116_despliegue_aplicacion_docker/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hola Mundo!"))
+	})
+
+	http.ListenAndServe(":8000", nil)
+
+}
